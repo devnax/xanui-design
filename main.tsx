b8ad1483d2840907778507338d15e8f45a33b3cd
Naxrul Ahmed <devnaxrul@gmail.com>
2026-05-13 00:51:49 +0600

    initial

---
+++ main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        fontFamily: "monospace,math, sans-serif",
+        textAlign: "center",
+        marginTop: "50px",
+      }}
+    >
+      aasd
+    </div>
+  );
+};
+const rootEle = document.getElementById("root");
+if (rootEle) {
+  const root = createRoot(rootEle);
+  root.render(<App />);
+}
